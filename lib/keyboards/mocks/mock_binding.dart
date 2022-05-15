8e3658c7a7b81b27ed3911136c4242c1f3a8fbeb
jgwei <weijanguo@qq.com>
2022-05-15 14:20:53 +0800

    fix flutter WidgetsBinding.instance error

--- lib/keyboards/mocks/mock_binding.dart
+++ lib/keyboards/mocks/mock_binding.dart
@@ -1,26 +1,34 @@
 part of cool_ui;
 
-
-
 class MockBinding extends WidgetsFlutterBinding {
   @override
   void initInstances() {
     // TODO: implement initInstances
     super.initInstances();
-    _binaryMessenger  = MockBinaryMessenger(super.defaultBinaryMessenger);
+    _binaryMessenger = MockBinaryMessenger(super.defaultBinaryMessenger);
   }
 
-  
   static WidgetsBinding ensureInitialized() {
-    if (WidgetsBinding.instance == null)
-      MockBinding();
-    return WidgetsBinding.instance!;
+    // if (WidgetsBinding.instance == null)
+    //   MockBinding();
+    // return WidgetsBinding.instance!;
+    return MockBinding.instance;
   }
+
+  static WidgetsBinding get instance {
+    _instance ??= MockBinding();
+    return WidgetsBinding.instance;
+  }
+
+  static WidgetsBinding? _instance;
 
   MockBinaryMessenger? _binaryMessenger;
+
   @override
   BinaryMessenger get defaultBinaryMessenger {
-    return _binaryMessenger != null ? _binaryMessenger! : super.defaultBinaryMessenger;
+    return _binaryMessenger != null
+        ? _binaryMessenger!
+        : super.defaultBinaryMessenger;
   }
 }
 
